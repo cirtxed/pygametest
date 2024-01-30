02188b877f7a51d711197efcab49a9c17b21a34f
liltrip <84157256+cirtxed@users.noreply.github.com>
2024-01-29 21:57:31 -0500

    Update player.py

--- player.py
+++ player.py
@@ -1,5 +1,17 @@
 import pygame
 
+pygame.font.init()
+
+font_size = 30
+font = pygame.font.Font('freesansbold.ttf', 30)
+
+def draw_text(surface, text, font, color, x, y):
+    text_surface = font.render(text, True, color)
+    text_rect = text_surface.get_rect()
+    text_rect.topleft = (x, y)
+    surface.blit(text_surface, text_rect)
+
+
 class Player:
     def __init__(self, pos, display):
         self.x = pos.x
@@ -20,32 +32,26 @@ class Player:
         elif keys[pygame.K_a]:
             self.speed_x -= 1 
 
-        touching = False
-
+        if self.x >= center_x + 10 and self.scroll_x > 0:
+            self.scroll_x += self.speed_x
+        else:
+            self.scroll_x = 0.1
+            self.x += self.speed_x
 
         if self.touch_tile(tiles):
-            #self.speed_x *= -1.0
-            touching = True
-            pass
+            if self.x >= center_x + 10 and self.scroll_x > 0:
+                self.speed_x *= -1
+                self.scroll_x += self.speed_x
+                self.speed_x = 0
+            else:
+                self.scroll_x = 0.1
+                self.speed_x *= -1
+                self.x += self.speed_x
+                self.speed_x = 0
         else:
             self.speed_x *= 0.7
 
-        if self.x >= center_x + 10 and self.scroll_x > 0:
-            if touching:
-                self.scroll_x += self.speed_x * -1
-                self.speed_x = 0
-                #self.scroll_x += self.speed_x
-            else:
-                self.scroll_x += self.speed_x
-        else:
-            self.scroll_x = 0.1
-            if touching:
-                self.x += self.speed_x * -1
-                self.speed_x = 0
-                #self.x += self.speed_x
-            else:
-                self.x += self.speed_x
-            print(self.speed_x)
+        
 
     def gravity(self, tiles):  #, tiles
         self.y -= self.force
@@ -70,4 +76,5 @@ class Player:
     def update(self, tiles):
         self.gravity(tiles)
         self.movement(tiles)
+        draw_text(self.display, f'{self.speed_x}', font, (255, 55, 50), 600, 75)
         pygame.draw.rect(self.display, (50, 255, 50), self.get_rect())
